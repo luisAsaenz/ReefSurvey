bf6f1a2e05c80053fbc0880f663c8424531ed3dd
James Contreras <james.a.contreras@outlook.com>
2020-03-12 10:42:47 -0400

    deleted something

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ namespace ReefSurvey
 			
 			string line = "Gulf of Mexico,Flower Garden Banks,East Bank,2011,GULF-FGBS-0800-1100,6379,8/2/2011,27.91092,-93.60346,FGBNMS,HARD                                              ,Carangidae,Carangoides ruber,Bar Jack,P,45,1";
 			
-
-
 			Parse.Parsetest(line);
 		}
 	}
