05a62c450bafa981e17d82d10bf344626d01c0c6
James Contreras <james.a.contreras@outlook.com>
2020-03-12 10:40:06 -0400

    Program and Parse

--- Parse.cs
+++ Parse.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace ReefSurvey
 {
     class Parse
     {
-       public static void Parsetest()
-       {
-            string phrase = "The quick brown cow";
-            string[] words = phrase.Split(' ');
+        public static string[] Parsetest(string line)
+        {
+            string[] result = new string[17];
+            string[] words = line.Split(',');
 
-            foreach (var word in words) 
+            int i = 0;
+            foreach (var word in words)
             {
-                System.Console.WriteLine($"<{word}>");
+                word.TrimEnd();
+                result[i] = word;
+                i++;
             }
 
-       }
-            
-            
+            foreach (var s in result)
+            {
+                s.TrimEnd();
+                Console.Write($"{s},");
+            }
+            return result;
 
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace ReefSurvey
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
+			string line = "Gulf of Mexico,Flower Garden Banks,East Bank,2011,GULF-FGBS-0800-1100,6379,8/2/2011,27.91092,-93.60346,FGBNMS,HARD                                              ,Carangidae,Carangoides ruber,Bar Jack,P,45,1";
+			
 
-			Parse.Parsetest();
+
+			Parse.Parsetest(line);
 		}
 	}
 }
