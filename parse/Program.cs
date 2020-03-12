a124e587ffd9ad7b416d7af6102446b6067cc99e
James Contreras <james.a.contreras@outlook.com>
2020-03-12 08:50:33 -0400

    Parse initial

---
+++ parse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parse();
+        }
+
+        public static void Parse()
+        {
+            
+        }
+    }
+}
