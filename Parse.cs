223fed47651e024a1705f89b8846724dc045228a
James Contreras <james.a.contreras@outlook.com>
2020-03-12 09:02:26 -0400

    update to Parser

---
+++ Parse.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.ExtendedProperties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReefSurvey
+{
+    class Parse
+    {
+       public static void Parsetest()
+       {
+            string phrase = "The quick brown cow";
+            string[] words = phrase.Split(' ');
+
+            foreach (var word in words) ;
+            {
+                System.Console.WriteLine($"<{words}>");
+            }
+
+       }
+            
+            
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ReefSurvey
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+
+			Parse.Parsetest();
 		}
 	}
 }
